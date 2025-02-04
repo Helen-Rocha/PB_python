cbca857051193ac0c4247b1355e15f859cf7f828
Helen-Rocha <helenrocha0716@gmail.com>
2025-02-03 21:03:19 -0600

    Add files via upload

---
+++ Build/Problema_8.py
@@ -0,0 +1,5 @@
+#Calcular el área y circunferencia de un circulo
+radio=float(input("Ingrese el radio del circulo:"))
+area = 3.1416*(radio)**2
+circun = 2*3.1416*radio
+print("El área es",area, "y la circunferencia",circun)
